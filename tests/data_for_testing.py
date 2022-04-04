a014f32c9c86d1a501cdd115777ec21a2b79c48c
Guillaume Postec <gg.postec@laposte.net>
2022-04-04 17:00:53 +0200

    Add initialized data for tests with reset methods

---
+++ tests/data_for_testing.py
@@ -0,0 +1,45 @@
+from server import competitions, clubs
+
+INITIAL_NB_OF_PLACES = 25
+INITIAL_NB_OF_POINTS = 30
+TEST_CLUB_NAME = 'test_club'
+TEST_COMPETITION_NAME = 'test_competition'
+
+
+def add_competition_for_tests():
+    test_competition = {
+        'name': TEST_COMPETITION_NAME,
+        'date': '2020-03-27 10:00:00',
+        'numberOfPlaces': INITIAL_NB_OF_PLACES,
+        'attendingClubs': {}
+    }
+    competitions.append(test_competition)
+
+
+def add_club_for_tests():
+    test_club = {
+        'name': TEST_CLUB_NAME,
+        'email': 'admin@test_club.com',
+        'points': INITIAL_NB_OF_POINTS
+    }
+    clubs.append(test_club)
+
+
+def reset_test_club_points():
+    club = [c for c in clubs if c['name'] == 'test_club'][0]
+    club['points'] = INITIAL_NB_OF_POINTS
+
+
+def reset_test_competition_nb_of_places():
+    competition = [c for c in competitions if c['name'] == 'test_competition'][0]
+    competition['numberOfPlaces'] = INITIAL_NB_OF_PLACES
+
+
+def reset_test_club_attendees_to_test_competition():
+    competition = [c for c in competitions if c['name'] == 'test_competition'][0]
+    competition['attendingClubs']['test_club'] = 0
+
+
+def set_10_test_club_attendees_to_test_competition():
+    competition = [c for c in competitions if c['name'] == 'test_competition'][0]
+    competition['attendingClubs']['test_club'] = 10
