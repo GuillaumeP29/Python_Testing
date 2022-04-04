c9b55f1e6f17439ba1832487d546aa99fbace2f9
Guillaume Postec <gg.postec@laposte.net>
2022-04-04 12:41:24 +0200

    tests flash messages and 12 places exceeding

--- tests/unit_tests/test_purchase_places.py
+++ tests/unit_tests/test_purchase_places.py
@@ -1,4 +1,5 @@
 import pytest
+from flask import get_flashed_messages
 
 from server import app
 
@@ -18,3 +19,31 @@ def test_purchase_places_should_decrease_nb_of_places(client):
         })
     p_to_find = f'<p>Number of Places: {new_nb_of_places_expected}</p>'
     assert p_to_find in str(response.data)
+
+
+def test_purchase_places_should_send_succes_message(client):
+    nb_of_places_to_buy = 3
+    client.post('/purchasePlaces/', data={
+        'competition': 'Spring Festival', 'club': 'Simply Lift', 'places': str(nb_of_places_to_buy)
+        })
+    message = get_flashed_messages()
+    assert message[0] == 'Great-booking complete!'
+
+
+def test_purchase_places_should_not_allow_more_than_12_places_per_club(client):
+    initial_nb_of_places = 13
+    nb_of_places_to_buy = 3
+    response = client.post('/purchasePlaces/', data={
+        'competition': 'Spring Festival', 'club': 'Simply Lift', 'places': str(nb_of_places_to_buy)
+        })
+    p_to_find = f'<p>Number of Places: {initial_nb_of_places}</p>'
+    assert p_to_find in str(response.data)
+
+
+def test_purchase_places_should_send_error_message_for_more_than_12_places_per_club(client):
+    nb_of_places_to_buy = 3
+    client.post('/purchasePlaces/', data={
+        'competition': 'Spring Festival', 'club': 'Simply Lift', 'places': str(nb_of_places_to_buy)
+        })
+    message = get_flashed_messages()
+    assert message[0] == 'Simply Lift would have more than 12 places-booking refused!'
